bdcdb9683ea9f20656752026721c96c451d532c2
Jason Bernzweig <jasonbernzweig@jasons-air.lan>
2023-04-07 12:33:27 -0400

    Updates to JS

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -15,9 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 
 // GET route for homepage
 app.get('/', (req, res) => {
-    res.send('Hello World!') // REVISIT
+    res.send(path.join(__dirname, '/public/index.html'))
 }) 
 
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for retrieving all the tips
+app.get('/api/tips', (req, res) => {
+  console.info(`${req.method} request received for tips`);
+  readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+});
 
 
 
